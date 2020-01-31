29bd9f4da3da03b6a3f5b3639fd81402c4377890
Alex Wang <j3ang@gmail.com>
2020-01-31 03:15:51 -0600

    update script

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -10,17 +10,9 @@ usermod -aG docker ubuntu
 curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# copy the dockerfile into /srv/docker 
-# if you change this, change the systemd service file to match
-# WorkingDirectory=[whatever you have below]
-mkdir /usr/src/app
-curl -o /usr/src/app/docker-compose.yml https://raw.githubusercontent.com/j3ang/TID-KEY/master/docker-compose.yml
-
 # copy in systemd unit file and register it so our compose file runs 
 # on system restart
 curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/j3ang/TID-KEY/master/docker-compose-app.service
 systemctl enable docker-compose-app
 
-# start up the application via docker-compose
-docker-compose -f /usr/src/app/docker-compose.yml up -d
-
+docker pull twinimage/tid-key:v1.0.0
