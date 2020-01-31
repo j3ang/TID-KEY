3f52a6b49bd1f3bf38a2ee8171a77eda0af17bf6
Alex Wang <j3ang@gmail.com>
2020-01-31 02:32:08 -0600

    update path

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -22,5 +22,5 @@ curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercon
 systemctl enable docker-compose-app
 
 # start up the application via docker-compose
-docker-compose -f /srv/docker/docker-compose.yml up -d
+docker-compose -f /usr/src/app/docker-compose.yml up -d
 
---
+++ lunch-script.sh
@@ -0,0 +1,3 @@
+curl -o lightsail-compose.sh https://raw.githubusercontent.com/j3ang/TID-KEY/master/lightsail-compose.sh
+chmod +x ./lightsail-compose.sh
+./lightsail-compose.sh
