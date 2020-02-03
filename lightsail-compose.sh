61eb739c43891140f509e00f842b5abe4b9659ea
Alex Wang <j3ang@gmail.com>
2020-02-03 01:18:00 -0600

    updatre script

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -6,9 +6,6 @@ curl -sSL https://get.docker.com | sh
 # make it so you don't need to sudo to run docker commands
 usermod -aG docker ubuntu
 
-# update system packeges
-apt-get update
-
 # install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
@@ -23,5 +20,5 @@ workdir=/usr/src/app
 mkdir $workdir | cd $workdir
 
 # clone latest code 
-git clone https://github.com/j3ang/TID-KEY.git | cd TID-KEY
+git clone https://github.com/j3ang/TID-KEY.git ; cd TID-KEY
 docker pull twinimage/tid-key:v1.0.0
